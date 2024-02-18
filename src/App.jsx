a7b84243225a82be9acd72ae659ecbc032b1b6b0
AndroIssaq <androashk88@gmail.com>
2024-02-18 15:36:41 +0200

    editing the her section 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,14 @@
+/* eslint-disable no-unused-vars */
 import "./App.css";
 import { useTranslation } from "react-i18next";
 import { Routes, Route } from "react-router";
 import Home from "./pages/Home";
 import NavBar from "./components/NavBar";
-<<<<<<< HEAD
-
-=======
-import Hero from "./components/Hero";
->>>>>>> 4d5c214024e9f4f512192db6dd5a236861b9743f
 function App() {
   const [t, i18n] = useTranslation();
   return (
     <>
       <NavBar />
-<<<<<<< HEAD
-=======
-      <Hero />
->>>>>>> 4d5c214024e9f4f512192db6dd5a236861b9743f
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>

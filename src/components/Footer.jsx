58a3bab77960cc9b7109e63597b1a97769ace3f8
AndroIssaq <androashk88@gmail.com>
2024-03-18 18:37:28 +0200

    ربلبلبل

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,7 +10,7 @@ import {
   FaInstagramSquare,
 } from "react-icons/fa";
 const Footer = () => {
-  const { i18n } = useTranslation();
+  const [t,i18n]   = useTranslation();
   const lang = i18n.language;
   const location = useLocation();
   const date = new Date();
@@ -27,76 +27,75 @@ const Footer = () => {
           <div className="space-y-8 xl:col-span-1">
             <h1 className="text-[35px]">
               <Link to={"/"}>
-                world <span className="text-white">gym</span>
+                world <span className="text-white text-[35px]">gym</span>
               </Link>
             </h1>
-            <p className="text-base leading-6">
-              Your Path to a Healthier Lifestyle and Greater Well-being.
+            <p className=" text-[25px] leading-6">
+             {t('Your Path to a Healthier Lifestyle and Greater Well-being')}
             </p>
             <div className="flex space-x-6"></div>
           </div>
           <div className="mt-12 grid grid-cols-1 gap-8 xl:mt-0 xl:col-span-2">
-            <div className="md:grid md:grid-cols-2 md:gap-8">
+            <div className={`${lang==='ar'&&'text-right'} md:grid md:grid-cols-2 md:gap-8`}>
               <div>
-                <h4 className="text-lg font-semibold tracking-wider text-mainColor  uppercase">
-                  Contact
+                <h4 className=" text-[35px] font-semibold tracking-wider text-mainColor  uppercase">
+                {t('CONTACT')}
                 </h4>
-                <ul className="mt-4 space-y-4">
-                  <li>
-                    <h5 className="text-base leading-6">Address</h5>
-                    <p className="mt-2 text-base leading-6">
-                    Abu Dhabi Corniche in front of the Mall / Souq Tower. Floor first
-                    </p>
+                <ul className="mt-4 space-y-4 ">
+                  <li className={`${lang==='ar'&&'text-right'}`}>
+                    <h5 className=" leading-6 text-[25px] "> {t('Address')} <span className="mt-2 font-light  text-[20px] leading-6">
+                    : {t('Abu Dhabi Corniche in front of the Mall / Souq Tower. Floor first')}
+                    </span></h5>  
                   </li>
-                  <li>
-                    <h5 className="text-base leading-6">Phone</h5>
-                    <p className="mt-2 text-base leading-6">
-                    026220202
-                    </p>
+                  <li className={`${lang==='ar'&&'text-right'}`}>
+                    <h5 className="text-[25px] leading-6">{t('Phone')} <span className="mt-2 text-[20px] leading-6">
+                    : 026220202
+                    </span></h5>
+                    
                   </li>
-                  <li>
-                    <h5 className="text-base leading-6">Mail Address</h5>
-                    <p className="mt-2 text-base leading-6">
-                      info@worldgym.com
-                    </p>
+                  <li className={`${lang==='ar'&&'text-right'}`}>
+                    <h5 className="text-[25px] leading-6">{t('Mail Address')}  </h5> <span className="mt-2 text-[20px] leading-6">
+                      : info@worldgym.com
+                    </span>
+                   
                   </li>
                 </ul>
               </div>
               <div className="md:mt-0">
-                <h4 className="text-lg  font-semibold tracking-wider text-mainColor uppercase">
-                  Quick Links
+                <h4 className="text-[35px]  font-semibold tracking-wider text-mainColor uppercase">
+                {t('QUICK LINKS')}
                 </h4>
                 <ul className="mt-4 space-y-4">
-                  <li>
+                  <li className={`${lang==='ar'&&'text-right'}`}>
                     <a
-                      className="text-base leading-6 hover:text-mainColor"
+                      className="text-[25px] leading-6 hover:text-mainColor"
                       href="#"
                     >
-                      <Link to={"/"}>Home</Link>
+                      <Link to={"/"}> {t('Home')}</Link>
                     </a>
                   </li>
-                  <li>
+                  <li className={`${lang==='ar'&&'text-right'}`}>
                     <a
-                      className="text-base leading-6 hover:text-mainColor"
+                      className="text-[25px] leading-6 hover:text-mainColor"
                       href="#"
                     >
-                      <Link to={"/"}>Classes</Link>
+                      <Link to={"/Classes"}> {t('Classes')}</Link>
+                    </a>
+                  </li  >
+                  <li className={`${lang==='ar'&&'text-right'}`}>
+                    <a
+                      className="text-[25px] leading-6 hover:text-mainColor"
+                      href="#"
+                    >
+                      <Link to={"/AboutUs"}> {t('About Us')}</Link>
                     </a>
                   </li>
-                  <li>
+                  <li className={`${lang==='ar'&&'text-right'}`}>
                     <a
-                      className="text-base leading-6 hover:text-mainColor"
+                      className="text-[25px] leading-6 hover:text-mainColor"
                       href="#"
                     >
-                      <Link to={"/AboutUs"}>About</Link>
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      className="text-base leading-6 hover:text-mainColor"
-                      href="#"
-                    >
-                      <Link to={"/ContactUs"}>Contact</Link>
+                      <Link to={"/ContactUs"}> {t('ContactUs')}</Link>
                     </a>
                   </li>
                 </ul>
@@ -119,7 +118,7 @@ const Footer = () => {
               <FaInstagramSquare className="h-6 w-6" />
             </a>
           </div>
-          <p className="mt-8 text-base leading-6 text-gray-400 md:mt-0 md:order-1">
+          <p className="mt-8 text-[25px] leading-6 text-gray-400 md:mt-0 md:order-1">
             © {year} WorldGym. All rights reserved.
           </p>
         </div>
